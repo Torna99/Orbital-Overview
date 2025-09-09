1837452e977b95133b668df8a9830015fd5e66b7
Torna <tornaenri@gmail.com>
2025-09-09 11:08:23 +0200

    Multiple satellite track added

--- orbital-viewer.py
+++ orbital-viewer.py
@@ -1,33 +1,51 @@
+
+# WiP: questo codice richeide di passare come argomento del main le stazioni che si voglio visualizzare con 
+# necessità di conoscerli a priori e "bruttezza" nel passare gli argomenti tramite doppie virgolette => sistemare 
+# 
+
 import src.tle_loader as tle_loader
 import src.orbit_simulator as orbit_simulator
 import src.visualizer as visualizer
 import os.path
+import sys
 
 
 STATIONS_TLE_URL = "https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&amp;FORMAT=tle" 
-STATION = "ISS (ZARYA)"
+DEFAULT_STATION = "ISS (ZARYA)"
 ORBIT_MINUTES = 90
 
 def main():
 
-    all_tles = tle_loader.get_all_tle_files()
+    # create Earth canvas
+    fig, ax = visualizer.create_2D_earth()
 
-    # Downloading the TLEs if not present in the directory data
+    # Getting satellites to plot or the default
+    satellites = []
+    satellites = sys.argv[1:]
+    if len(satellites) == 0: 
+        print(f"No satellite selected: showing {DEFAULT_STATION}")
+        satellites.append(DEFAULT_STATION)
+
+    # Downloading the TLEs file if not present in the directory data
+    all_tles = tle_loader.get_all_tle_files()
     if not all_tles: 
         tle_loader.download_tle(STATIONS_TLE_URL)
-        print(f"{STATION}'s TLE downloaded!")
+        print(f"{satellite}'s TLE downloaded!")
     else: 
         print("data dir not empty!")
 
-    # choose orbital object TODO
+    for satellite in satellites:
 
-    tle = tle_loader.get_tle(STATION)
+        tle = tle_loader.get_tle(satellite)
+        
+        # compute orbits  
+        lats, lons = orbit_simulator.calculate_orbit(tle, ORBIT_MINUTES)
 
-    # compute orbits  
-    lats, lons = orbit_simulator.calculate_orbit(tle, ORBIT_MINUTES)
+        # plotting orbit
+        visualizer.track_2D_orbit(fig, ax, lats, lons, satellite)
 
-    # plotting orbit
-    visualizer.plot_2D_orbit(lats, lons, STATION)
+    visualizer.show_plot()
+
 
 if __name__ == "__main__":
     main()
--- src/visualizer.py
+++ src/visualizer.py
@@ -2,27 +2,42 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 from matplotlib.animation import FuncAnimation
 
-def plot_2D_orbit(lats, lons, obj_name, title="2D Ground Track"):
+# setting 2D figure
+# # create a new plot of 12x6 inches and add axes
+# fig = plt.figure(figsize=(12,6))
+# ax = plt.axes(projection=ccrs.PlateCarree())
 
+# # basic earth map 
+# ax.stock_img()
+# ax.coastlines()
+
+def create_2D_earth():
     # create a new plot of 12x6 inches and add axes
-    plt.figure(figsize=(12,6))
+    fig = plt.figure(figsize=(12,6))
     ax = plt.axes(projection=ccrs.PlateCarree())
 
     # basic earth map 
     ax.stock_img()
     ax.coastlines()
 
-    # plot the trajectory 
-    ax.plot(lons, lats, 'r-', linewidth=2, label=f"{obj_name} Track", transform=ccrs.Geodetic())
+    return fig, ax
 
+def track_2D_orbit(fig, ax, lats, lons, obj_name, title="2D Ground Track"):
+
+    # plot the trajectory 
+    ax.plot(lons, lats, '-', linewidth=2, label=f"{obj_name} Track", transform=ccrs.Geodetic())
 
     # Highlight starting and end point
-    ax.plot(lons[0], lats[0], 'go', markersize=8, label="Start")
-    ax.plot(lons[-1], lats[-1], 'bo', markersize=8, label="End")
+    ax.plot(lons[0], lats[0], 'o', markersize=8, label=f"{obj_name} Start")
+    ax.plot(lons[-1], lats[-1], 'o', markersize=8, label=f"{obj_name} End")
 
     plt.title(title, fontsize=14)
     plt.legend()
 
+    return fig, ax
+
+# TODO: i don't like this way
+def show_plot():
     plt.show()
 
 
